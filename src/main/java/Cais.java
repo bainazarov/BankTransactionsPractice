495b3b9353282c2782b0eaf5c25b4fb9135ada1c
Abdurakhmon Bainazarov <baynazarowv@gmail.com>
2023-07-21 22:29:02 +0400

    Initial commit

---
+++ src/main/java/Cais.java
@@ -0,0 +1,8 @@
+public class Cais {
+    private String id;
+    private String startDate;
+    private String endDate;
+    private String accountType;
+    private CreditLimit creditLimit;
+    private List<CaisDetails> caisDetailsList;
+}
